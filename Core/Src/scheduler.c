52e16b61f4374388601bed1cd89ff2008d2cd4d0
NPAnh1109 <104689229+NPAnh1109@users.noreply.github.com>
2023-12-11 01:40:39 +0700

    Final commit. Done project

---
+++ Core/Src/scheduler.c
@@ -0,0 +1,59 @@
+/*
+ * scheduler.c
+ *
+ *  Created on: Nov 27, 2023
+ *      Author: 84977
+ */
+
+#include "scheduler.h"
+
+static uint32_t idx = 0;
+static struct Node* head = NULL;
+
+void SCH_Init(){
+	clearList(head);
+	head = NULL;
+	idx = 0;
+};
+
+uint32_t SCH_Add_Task(CALLBACK callback, uint16_t Delay, uint16_t Period){
+	if(idx == SCH_MAX_TASKS - 1) return SCH_MAX_TASKS;
+	Task_TypeDef_t data;
+	data.callback = callback;
+	data.Delay = Delay;
+	data.Period = Period;
+	data.Runme = 0;
+	data.taskID = idx++;
+	addTask(&head, data);
+	return data.taskID;
+};
+
+void SCH_Update(){
+	if(head == NULL) return;
+	if(head->data.Delay <= 0){
+		head->data.Runme = 1;
+	}else{
+		head->data.Delay--;
+	}
+};
+
+void SCH_Dispatch_Tasks(){
+	if(head == NULL) return;
+	if(head->data.Runme == 1){
+		(*head->data.callback)();
+		Task_TypeDef_t reNewTask;
+		reNewTask.Delay = head->data.Period;
+		reNewTask.Period = head->data.Period;
+		reNewTask.Runme = 0;
+		reNewTask.callback = head->data.callback;
+		reNewTask.taskID = head->data.taskID;
+		deleteTask(&head);
+		addTask(&head, reNewTask);
+	}
+};
+
+void SCH_Delete_Task(uint32_t taskID) {
+	deleteTaskID(&head, taskID);
+};
+
+
